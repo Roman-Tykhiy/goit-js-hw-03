13164d7d96beb8f414570be8db3b2aa61e9c2c37
Roman_Tykhiy <2rstykhiy@gmail.com>
2024-10-06 11:13:05 +0300

    HW3

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,13 @@
-function makeTransaction (quantity, pricePerDroid) {
-let totalPrice = quantity * pricePerDroid;
-return(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
-};
+function slugify(title) {
+    const slug = title.toLowerCase();
+    const slugM = slug.split(" ")
+    return slugM.join("-")
+    
+    
+}
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+
+console.log(slugify("Arrays for beginners"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
